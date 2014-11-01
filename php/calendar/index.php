94c57264044736fd0a4330efeb7a9be0b230115f
takryou79 <takryou79@gmail.com>
2014-11-02 01:36:45 +0900

    commit from webmatrix

--- php/calendar/index.php
+++ php/calendar/index.php
@@ -61,7 +61,7 @@
       <br>
       <br>
       <form name="form2" method="POST" action="index.php">
-        edit from github
+        edit from webmatrix
         <br>
         <br>
         <input type="submit" value="送信">
